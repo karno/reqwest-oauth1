fb7445fca9566c21487ae472d629ce053867b441
Karno <karnoroid@gmail.com>
2020-10-04 16:21:21 +0900

    update the documentation of errors.
    (cherry picked from commit b0d9a17f3c92d9ea7c0e04570f8f6c03c1458074)

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub type SignResult<T> = std::result::Result<T, SignError>;
 pub type TokenReaderResult<T> = std::result::Result<T, TokenReaderError>;
 
+/// The Error bundles the TokenReaderError, SignError, and reqwest::Error.
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("token acquisition failed : {0}")]
@@ -14,20 +15,26 @@ pub enum Error {
     Reqwest(#[from] reqwest::Error),
 }
 
+/// Errors about the signing with OAuth1 protocol.
 #[derive(Error, Debug, Clone)]
 pub enum SignError {
+    /// Specified oauth_* parameter is not existed in the protocol specification.
     #[error("unknown oauth parameter : {0}")]
     UnknownParameter(String),
+    /// Specified oauth_* parameter is not configured via the reqwest::RequestBuilder::(query/form).
     #[error("specified parameter {0} could not be configured via the reqwest parameters.")]
     UnconfigurableParameter(String),
+    /// An invalid value is specified as the oauth_timestamp parameter.
     #[error("invalid oauth_timestamp, must be u64, but {0} is not compatible.")]
     InvalidTimestamp(String),
+    /// An invalid value is specified as the oauth_version parameter.
     #[error("invalid oauth_version, must be 1.0 or just empty, but specified {0}.")]
     InvalidVersion(String),
 }
 
 #[derive(Error, Debug, Clone)]
 pub enum TokenReaderError {
-    #[error("response has malformed format: not found {0} in {1}")]
+    /// Returned value could not be parsed in the TokenReader.
+    #[error("the response has malformed format: key {0} is not found in response {1}")]
     TokenKeyNotFound(&'static str, String),
 }
