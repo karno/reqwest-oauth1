bd96ed989fd0e876f5ea0da4e10d0a44f1f90867
Karno <karnoroid@gmail.com>
2020-10-04 17:19:36 +0900

    remove type parameters from secrets.
    That is rarely required and only work for introducing redundant complexity.

--- src/secrets.rs
+++ src/secrets.rs
@@ -39,61 +39,62 @@ let req = reqwest::Client::new()
 
 */
 #[derive(Debug, Clone)]
-pub struct Secrets<'a, T> {
-    token: T,
-    token_secret: T,
-    consumer_key: Cow<'a, str>,
-    consumer_secret: Cow<'a, str>,
+pub struct Secrets<'a> {
+    consumer_key_secret: (Cow<'a, str>, Cow<'a, str>),
+    token_key_secret: Option<(Cow<'a, str>, Cow<'a, str>)>,
 }
 
-impl<'a> Secrets<'a, ()> {
+impl<'a> Secrets<'a> {
     pub fn new<TKey, TSecret>(consumer_key: TKey, consumer_secret: TSecret) -> Self
     where
         TKey: Into<Cow<'a, str>>,
         TSecret: Into<Cow<'a, str>>,
     {
         Secrets {
-            token: (),
-            token_secret: (),
-            consumer_key: consumer_key.into(),
-            consumer_secret: consumer_secret.into(),
+            consumer_key_secret: (consumer_key.into(), consumer_secret.into()),
+            token_key_secret: None,
         }
     }
 
-    pub fn token<TKey, TSecret>(
-        self,
-        token: TKey,
-        token_secret: TSecret,
-    ) -> Secrets<'a, Cow<'a, str>>
+    pub fn new_with_token<TKey, TSecret, TToken, TTokenSecret>(
+        consumer_key: TKey,
+        consumer_secret: TSecret,
+        token: TToken,
+        token_secret: TTokenSecret,
+    ) -> Self
+    where
+        TKey: Into<Cow<'a, str>>,
+        TSecret: Into<Cow<'a, str>>,
+        TToken: Into<Cow<'a, str>>,
+        TTokenSecret: Into<Cow<'a, str>>,
+    {
+        Secrets {
+            consumer_key_secret: (consumer_key.into(), consumer_secret.into()),
+            token_key_secret: Some((token.into(), token_secret.into())),
+        }
+    }
+
+    pub fn token<TKey, TSecret>(self, token: TKey, token_secret: TSecret) -> Secrets<'a>
     where
         TKey: Into<Cow<'a, str>>,
         TSecret: Into<Cow<'a, str>>,
     {
         Secrets {
-            token: token.into(),
-            token_secret: token_secret.into(),
-            consumer_key: self.consumer_key,
-            consumer_secret: self.consumer_secret,
+            token_key_secret: Some((token.into(), token_secret.into())),
+            ..self
         }
     }
 }
 
-impl SecretsProvider for Secrets<'_, ()> {
+impl SecretsProvider for Secrets<'_> {
     fn get_consumer_key_pair<'a>(&'a self) -> (&'a str, &'a str) {
-        (&self.consumer_key, &self.consumer_secret)
+        (&self.consumer_key_secret.0, &self.consumer_key_secret.1)
     }
 
     fn get_token_pair_option<'a>(&'a self) -> Option<(&'a str, &'a str)> {
-        None
-    }
-}
-
-impl SecretsProvider for Secrets<'_, Cow<'_, str>> {
-    fn get_consumer_key_pair<'a>(&'a self) -> (&'a str, &'a str) {
-        (&self.consumer_key, &self.consumer_secret)
-    }
-
-    fn get_token_pair_option<'a>(&'a self) -> Option<(&'a str, &'a str)> {
-        Some((&self.token, &self.token_secret))
+        match &self.token_key_secret {
+            Some((k, s)) => Some((&k, &s)),
+            None => None,
+        }
     }
 }
