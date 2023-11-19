4586332f8a06b00a7a82e49a8fad71b1bdf922df
karno <karnoroid@gmail.com>
2023-11-19 07:28:39 +0000

    build on Nix.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  nixpkgs = builtins.fetchTarball "channel:nixos-unstable";
+  rust-overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"));
+  pkgs = import nixpkgs { overlays = [ rust-overlay ]; };
+  rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
+with pkgs;
+mkShell {
+  packages = [
+    lld
+    clang
+    rust-toolchain
+  ];
+  buildInputs = [
+    openssl
+  ];
+  # envs
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+}
