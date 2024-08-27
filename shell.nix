ec68b952ac3a922403a3f2dda44feeb08fb06b48
karno <karnoroid@gmail.com>
2024-08-27 23:55:36 +0000

    fix to use latest stable rust.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 let
-  nixpkgs = builtins.fetchTarball "channel:nixos-unstable";
   rust-overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"));
-  pkgs = import nixpkgs { overlays = [ rust-overlay ]; };
+  pkgs = import <nixpkgs> { overlays = [ rust-overlay ]; };
   rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 in
 with pkgs;
